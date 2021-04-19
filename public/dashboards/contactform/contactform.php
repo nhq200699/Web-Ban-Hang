0de0bc57b4ea71822ccd9ced1ba571d370375a42
bichhanhit <iamhanhbich@gmail.com>
2021-04-19 21:18:04 +0700

    from contact

--- public/dashboards/contactform/contactform.php
+++ public/dashboards/contactform/contactform.php
@@ -3,7 +3,6 @@
   * Requires the PHP Mail Form library
   * The PHP Mail Form library is available only in the pro version of the template
   * The library should be uploaded to: lib/php-mail-form/php-mail-form.php
-  * For more info and help: https://templatemag.com/php-mail-form/
   */
 
   if( file_exists($php_mail_form_library = '../lib/php-mail-form/php-mail-form.php' )) {
